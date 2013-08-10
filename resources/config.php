682defcea0cd7824a17bf0edf856865ce8be2cc9
Neil Barton <neil@roughcoder.com>
2013-08-10 17:38:29 +0100

    Initial Build

---
+++ resources/config.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+    The important thing to realize is that the config file should be included in every
+    page of your project, or at least any page you want access to these settings.
+    This allows you to confidently use these settings throughout a project because
+    if something changes such as your database credentials, or a path to a specific resource,
+    you'll only need to update it here.
+*/
+
+$config = array(
+    "db" => array(
+        "dev" => array(
+            "dbname" => "database1",
+            "username" => "dbUser",
+            "password" => "pa$$",
+            "host" => "localhost"
+        ),
+        "live" => array(
+            "dbname" => "database2",
+            "username" => "dbUser",
+            "password" => "pa$$",
+            "host" => "localhost"
+        )
+    ),
+    "urls" => array(
+        "baseUrl" => "http://domainname.com"
+    ),
+    "paths" => array(
+        "resources" => "/path/to/resources",
+        "images" => array(
+            "content" => $_SERVER["DOCUMENT_ROOT"] . "/images/content",
+            "layout" => $_SERVER["DOCUMENT_ROOT"] . "/images/layout"
+        )
+    )
+);
+
+/*
+    Creating constants for heavily used paths makes things a lot easier.
+    ex. require_once(LIBRARY_PATH . "Paginator.php")
+*/
+defined("LIBRARY_PATH")
+    or define("LIBRARY_PATH", realpath(dirname(__FILE__) . '/library'));
+
+defined("VIEW_PATH")
+    or define("VIEW_PATH", realpath(dirname(__FILE__) . '/library/view'));
+
+defined("MODEL_PATH")
+    or define("MODEL_PATH", realpath(dirname(__FILE__) . '/library/model'));
+
+defined("TEMPLATES_PATH")
+    or define("TEMPLATES_PATH", realpath(dirname(__FILE__) . '/templates'));
+
+/*
+    Error reporting.
+*/
+ini_set("error_reporting", "true");
+error_reporting(E_ALL|E_STRCT);
+
+?>
