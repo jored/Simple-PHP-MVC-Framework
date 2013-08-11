4ae4d1889949d1be528faf59978bcd9f2162a8dd
Neil Barton <neil@roughcoder.com>
2013-08-11 03:02:38 +0100

    Extend controllers with Base_Controller

--- resources/library/controller/base_controller.php
+++ resources/library/controller/base_controller.php
@@ -22,34 +22,17 @@ namespace controller;
 * @link     http://roughcoder.com
 */
 
-class Home extends Base_Controller
+class Base_Controller
 {
 
-    /**
-     * Home Page
-     */
-    public function home( )
-    {
-
-            global $template;
-
-            $setInIndexDotPhp = "Hey! I was set in the index.php file.";
-
-            // Must pass in variables (as an array) to use in template
-            $variables = array(
-                'setInIndexDotPhp' => $setInIndexDotPhp
-            );
-
-            $template->render("home", $variables);
-
-    }
+    public $page = '';
 
     /**
-     * About Page
+     * Construct Function
      */
-    public function about( )
+    public function __construct( )
     {
-        echo "about".$this->page;
+        $this->page = $_SERVER['REQUEST_URI'];
     }
 
 }
